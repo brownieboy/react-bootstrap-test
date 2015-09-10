d654dcedf7aa4793082c19f64861bc1b148c9a69
Michael Brown <brownieboy@gmail.com>
2015-09-10 15:36:04 +1000

    Some extra buttons

--- client/components/TestComponent.jsx
+++ client/components/TestComponent.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ButtonToolbar from 'react-bootstrap/lib/ButtonToolbar';
 import Button from 'react-bootstrap/lib/Button';
+import ButtonGroup from 'react-bootstrap/lib/ButtonGroup';
+import DropdownButton from 'react-bootstrap/lib/DropdownButton';
+import MenuItem from 'react-bootstrap/lib/MenuItem';
 
 export default class TestComponent extends React.Component {
   render() {
@@ -28,6 +31,16 @@ export default class TestComponent extends React.Component {
           {/* Deemphasize a button by making it look like a link while maintaining button behavior */}
           <Button bsStyle='link'>Link</Button>
         </ButtonToolbar>
+        <br />
+        <ButtonGroup>
+          <DropdownButton bsStyle="success" title="Dropdown">
+            <MenuItem key="1" id="menuItem1">Dropdown link 1</MenuItem>
+            <MenuItem key="2" id="menuItem2">Dropdown link 2</MenuItem>
+          </DropdownButton>
+          <Button bsStyle="info">Middle</Button>
+          <Button bsStyle="info">Right</Button>
+      </ButtonGroup>
+
       </div>
     );
   }
