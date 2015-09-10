f0508e27351fc010a7220511eed7c060a3be14e7
Michael Brown <brownieboy@gmail.com>
2015-09-10 16:32:59 +1000

    More button tweaks

--- client/components/TestComponent.jsx
+++ client/components/TestComponent.jsx
@@ -14,19 +14,19 @@ export default class TestComponent extends React.Component {
           <Button>Default</Button>
 
           {/* Provides extra visual weight and identifies the primary action in a set of buttons */}
-          <Button bsStyle='primary'>Primary</Button>
+          <Button bsStyle='primary' disabled>Primary</Button>
 
           {/* Indicates a successful or positive action */}
           <Button bsStyle='success'>Success</Button>
 
           {/* Contextual button for informational alert messages */}
-          <Button bsStyle='info'>Info</Button>
+          <Button bsStyle='info' href="http://www.theguardian.com">Guardian</Button>
 
           {/* Indicates caution should be taken with this action */}
           <Button bsStyle='warning'>Warning</Button>
 
           {/* Indicates a dangerous or potentially negative action */}
-          <Button bsStyle='danger'>Danger</Button>
+          <Button bsStyle='danger' active>Danger</Button>
 
           {/* Deemphasize a button by making it look like a link while maintaining button behavior */}
           <Button bsStyle='link'>Link</Button>
